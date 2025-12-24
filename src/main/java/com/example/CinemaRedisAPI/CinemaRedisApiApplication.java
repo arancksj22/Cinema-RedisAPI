f58b9f79d3a55d0776168164930d3df4a0d787df
arancksj22 <arancksj@gmail.com>
2025-12-24 17:37:51 +0530

    basic boilerplate

---
+++ src/main/java/com/example/CinemaRedisAPI/CinemaRedisApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.CinemaRedisAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CinemaRedisApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CinemaRedisApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/CinemaRedisAPI/CinemaRedisApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.CinemaRedisAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CinemaRedisApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
