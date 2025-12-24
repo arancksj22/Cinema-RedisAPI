76a21558230038b5532b166f3a5073a1c384b309
arancksj22 <arancksj@gmail.com>
2025-12-24 18:22:13 +0530

    service layer

--- src/main/java/com/example/CinemaRedisAPI/Service.java
+++ src/main/java/com/example/CinemaRedisAPI/Service.java
@@ -1,6 +1,26 @@
 package com.example.CinemaRedisAPI;
 
+import javax.swing.text.html.Option;
+import java.util.List;
+import java.util.Optional;
+
 public class Service {
 
+    private final MovieRepo repo;
 
+    Service(MovieRepo repo){
+        this.repo = repo;
+    }
+
+    public MovieModel getById(Long id){
+        return repo.findById(id).orElseThrow(() -> new RuntimeException("Movie not found"));
+    }
+
+    public List<MovieModel> getAll(){
+        return repo.findAll();
+    }
+
+    public Optional<MovieModel> create(MovieModel model){
+        return repo.save(model);
+    }
 }
