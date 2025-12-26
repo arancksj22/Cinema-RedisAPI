63260d82c9ce23013e96ee3ea7b005ccbe7d0953
arancksj22 <arancksj@gmail.com>
2025-12-26 18:41:42 +0530

    dependancy injection

--- src/main/java/com/example/CinemaRedisAPI/Controller.java
+++ src/main/java/com/example/CinemaRedisAPI/Controller.java
@@ -1,15 +1,29 @@
 package com.example.CinemaRedisAPI;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api")
 public class Controller {
 
+    private Service service;
+
+    Controller(Service service){
+        this.service = service;
+    }
+
     @GetMapping
     public String HelloTester(){
         return "Hello, the backend is working";
+    }
+
+    @GetMapping
+    public ResponseEntity<List<MovieModel>> getAllMovies(){
+
     }
 }
