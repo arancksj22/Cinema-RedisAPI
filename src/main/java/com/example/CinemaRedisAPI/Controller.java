68b1f0fde77da25a1185370bf716f2135c799009
arancksj22 <arancksj@gmail.com>
2025-12-26 18:52:47 +0530

    controller done

--- src/main/java/com/example/CinemaRedisAPI/Controller.java
+++ src/main/java/com/example/CinemaRedisAPI/Controller.java
@@ -1,9 +1,7 @@
 package com.example.CinemaRedisAPI;
 
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -18,12 +16,24 @@ public class Controller {
     }
 
     @GetMapping
-    public String HelloTester(){
-        return "Hello, the backend is working";
+    public ResponseEntity<List<MovieModel>> getAllMovies(){
+        List<MovieModel> movies = service.getAll();
+        return ResponseEntity.ok(movies);
     }
 
-    @GetMapping
-    public ResponseEntity<List<MovieModel>> getAllMovies(){
+    @GetMapping("/{id}")
+    public ResponseEntity<MovieModel> getMovie(@PathVariable Long id){
+        return ResponseEntity.ok(service.getById(id));
+    }
 
+    @PostMapping({"/create","/save"})
+    public ResponseEntity<MovieModel> postMovie(@RequestBody MovieModel movie){
+        return ResponseEntity.ok(service.create(movie));
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public ResponseEntity<String> deleteMovie(@PathVariable Long id){
+        service.deleteById(id);
+        return ResponseEntity.ok("Deleted movie with id" + id);
     }
 }
