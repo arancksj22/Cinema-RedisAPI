ede63a619612b7687f841d57852c7e027587ca2d
arancksj22 <arancksj@gmail.com>
2026-01-01 22:46:06 +0530

    Redis Cache layer to Service

--- src/main/java/com/example/CinemaRedisAPI/Service.java
+++ src/main/java/com/example/CinemaRedisAPI/Service.java
@@ -1,5 +1,9 @@
 package com.example.CinemaRedisAPI;
 
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.cache.annotation.Caching;
+
 import javax.swing.text.html.Option;
 import java.util.List;
 import java.util.Optional;
@@ -13,18 +17,25 @@ public class Service {
         this.repo = repo;
     }
 
+    @Cacheable(value = "movie", key = "#id")
     public MovieModel getById(Long id){
         return repo.findById(id).orElseThrow(() -> new RuntimeException("Movie not found"));
     }
 
+    @Cacheable(value = "allMovies")
     public List<MovieModel> getAll(){
         return repo.findAll();
     }
 
+    @CacheEvict(value = "allMovies", allEntries = true)
     public MovieModel create(MovieModel model){
         return repo.save(model);
     }
 
+    @Caching(evict = {
+            @CacheEvict(value = "movie", key = "#id"),      // Remove specific movie
+            @CacheEvict(value = "allMovies", allEntries = true) // Remove list cache
+    })
     public void deleteById(Long id){
         repo.deleteById(id);
     }
